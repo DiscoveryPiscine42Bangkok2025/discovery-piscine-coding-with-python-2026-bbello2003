4eddcd54495b7154910aa36915432e22b0dd71da
bbello2003 <ppasikapong@gmail.com>
2026-02-10 17:55:49 +0700

    cell05/ex02-ex09

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for i in original_array:
+    new_array.append(i + 2)
+    
+unique_values = set(new_array)
+
+print(original_array)
+print(unique_values)
